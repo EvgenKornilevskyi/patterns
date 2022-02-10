ecc52e473884812a812ac5df0559b0a24f226878
Evgeniy Kornilevskiy <kornilevskiy@knu.ua>
2022-02-10 19:12:08 +0200

    how

--- CoffeeShop/CoffeeShop/ICoffeeType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CoffeeShop
-{
-    public interface ICoffeeType
-    {
-        public int CostPrice { get; set; }
-        public int SellPrice { get; set; }
-        public void DrinkCoffee();
-    }
-}

08b8c2b0d35272ccaace7d6ba881b47634753152
Evgeniy Kornilevskiy <kornilevskiy@knu.ua>
2022-06-06 21:10:23 +0300

    Builder Pattern Added

---
+++ Builder/Builder/Director.cs
@@ -0,0 +1,26 @@
+﻿using DesignPatterns.Builder;
+
+namespace DesingPatterns.Builder
+{
+    public class Director
+    {
+        private IBuilder? builder;
+        public IBuilder Builder
+        {
+            set { builder = value; }    
+        }
+        public void BuildStandartProduct()
+        {
+            var ingredients = new List<string>()
+            {
+                "Olives", "Tomatoes", "Sausages"
+            };
+            this.builder.SetDough("StandartPizza");
+            this.builder.SetIngredients(ingredients);
+        }
+        public void BuildProductWithNoIngredients()
+        {
+            this.builder.SetDough("NoIngredientsPizza");
+        }
+    }
+}
---
+++ Builder/Builder/IBuilder.cs
@@ -0,0 +1,9 @@
+﻿namespace DesignPatterns.Builder
+{
+    public interface IBuilder
+    {
+        void Reset();
+        void SetDough(string dough);
+        void SetIngredients(List<string> ingredients);
+    }
+}
